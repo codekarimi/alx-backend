c4aa484da734b106bd2a907c29698454e1acf39e
codekarimi <ckarimi676@gmail.com>
2024-09-25 12:49:51 +0000

    Node redis client and asyc operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,26 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('connect', () => console.log(`Redis client connected to the server`));
+client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+
+}
+
+async function displaySchoolValue(schoolName){
+    try{
+        const result = await getAsync(schoolName);
+        console.log(result);
+    } catch (err){
+        console.error(err.message);
+    }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
